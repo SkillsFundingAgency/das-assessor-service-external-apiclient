3f918e40fbc6fddc02bd2b91d97f4f052443bd02
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-08-02 11:17:28 +0100

    Added new unit tests

---
+++ src/SFA.DAS.AssessorService.ExternalApi.Core.Tests/Unit/Messages/Requests/Epa/DeleteEpaTests.cs
@@ -0,0 +1,115 @@
+﻿namespace SFA.DAS.AssessorService.ExternalApi.Core.Tests.Unit.Messages.Requests.Epa
+{
+    using FizzWare.NBuilder;
+    using NUnit.Framework;
+    using SFA.DAS.AssessorService.ExternalApi.Core.Messages.Request.Epa;
+    using System.Linq;
+
+    [TestFixture(Category = "Requests")]
+    public class DeleteEpaTests
+    {
+        [Test]
+        public void UlnInvalid()
+        {
+            // arrange
+            var request = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Uln = 12435).Build();
+
+            // act
+            bool isValid = request.IsValid(out var validationResults);
+
+            // assert
+            Assert.IsFalse(isValid);
+            Assert.That(validationResults, Has.Count.EqualTo(1));
+            StringAssert.AreEqualIgnoringCase("The apprentice's ULN should contain exactly 10 numbers", validationResults.First().ErrorMessage);
+        }
+
+        [Test]
+        public void NoStandardSpecified()
+        {
+            // arrange
+            var request = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Uln = 1243567890).With(r => r.Standard = null).Build();
+
+            // act
+            bool isValid = request.IsValid(out var validationResults);
+
+            // assert
+            Assert.IsFalse(isValid);
+            Assert.That(validationResults, Has.Count.EqualTo(1));
+            StringAssert.AreEqualIgnoringCase("A standard should be selected", validationResults.First().ErrorMessage);
+        }
+
+        [Test]
+        public void FamilyNameMissing()
+        {
+            // arrange
+            var request = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Uln = 1243567890).With(r => r.FamilyName = null).Build();
+
+            // act
+            bool isValid = request.IsValid(out var validationResults);
+
+            // assert
+            Assert.IsFalse(isValid);
+            Assert.That(validationResults, Has.Count.EqualTo(1));
+            StringAssert.AreEqualIgnoringCase("Enter the apprentice's last name", validationResults.First().ErrorMessage);
+        }
+
+        [Test]
+        public void EpaReferenceMissing()
+        {
+            // arrange
+            var request = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Uln = 1243567890).With(r => r.EpaReference = null).Build();
+
+            // act
+            bool isValid = request.IsValid(out var validationResults);
+
+            // assert
+            Assert.IsFalse(isValid);
+            Assert.That(validationResults, Has.Count.EqualTo(1));
+            StringAssert.AreEqualIgnoringCase("Enter the EPA reference", validationResults.First().ErrorMessage);
+        }
+
+        [Test]
+        public void WhenValid()
+        {
+            // arrange
+            var request = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Uln = 1243567890).Build();
+
+            // act
+            bool isValid = request.IsValid(out var validationResults);
+
+            // assert
+            Assert.IsTrue(isValid);
+            Assert.That(validationResults, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        [Category("IEquatable")]
+        public void WhenEqual()
+        {
+            // arrange
+            var request1 = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Standard = "1").Build();
+            var request2 = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Standard = "1").Build();
+
+            // act
+            bool areEqual = request1 == request2;
+
+            // assert
+            Assert.IsTrue(areEqual);
+        }
+
+        [Test]
+        [Category("IEquatable")]
+        public void WhenNotEqual()
+        {
+            // arrange
+            var request1 = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Standard = "1").Build();
+            var request2 = Builder<DeleteEpaRequest>.CreateNew().With(r => r.Standard = "9").Build();
+
+            // act
+            bool areNotEqual = request1 != request2;
+
+            // assert
+            Assert.IsTrue(areNotEqual);
+        }
+    }
+}
---
+++ src/SFA.DAS.AssessorService.ExternalApi.Core.Tests/Unit/Models/Learners/StatusTests.cs
@@ -0,0 +1,54 @@
+﻿namespace SFA.DAS.AssessorService.ExternalApi.Core.Tests.Unit.Models.Learners
+{
+    using FizzWare.NBuilder;
+    using NUnit.Framework;
+    using SFA.DAS.AssessorService.ExternalApi.Core.Models.Learners;
+
+    [TestFixture(Category = "Models")]
+    public class StatusTests
+    {
+        [Test]
+        public void WhenStatusValid()
+        {
+            // arrange
+            var status = Builder<Status>.CreateNew().With(s => s.CompletionStatus = 1).Build();
+
+            // act
+            bool isValid = status.IsValid(out var validationResults);
+
+            // assert
+            Assert.IsTrue(isValid);
+            Assert.That(validationResults, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        [Category("IEquatable")]
+        public void WhenStatusEqual()
+        {
+            // arrange
+            var status1 = Builder<Status>.CreateNew().Build();
+            var status2 = Builder<Status>.CreateNew().Build();
+
+            // act
+            bool areEqual = status1 == status2;
+
+            // assert
+            Assert.IsTrue(areEqual);
+        }
+
+        [Test]
+        [Category("IEquatable")]
+        public void WhenStatusNotEqual()
+        {
+            // arrange
+            var status1 = Builder<Status>.CreateNew().With(s => s.CompletionStatus = 1).Build();
+            var status2 = Builder<Status>.CreateNew().With(s => s.CompletionStatus = 2).Build();
+
+            // act
+            bool areNotEqual = status1 != status2;
+
+            // assert
+            Assert.IsTrue(areNotEqual);
+        }
+    }
+}
