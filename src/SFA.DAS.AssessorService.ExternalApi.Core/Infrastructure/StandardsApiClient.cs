bb2296d9ce03fbbbbea9265887faa2a613f4c309
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-08-05 09:14:05 +0100

    Corrected end points

--- src/SFA.DAS.AssessorService.ExternalApi.Core/Infrastructure/StandardsApiClient.cs
+++ src/SFA.DAS.AssessorService.ExternalApi.Core/Infrastructure/StandardsApiClient.cs
@@ -11,12 +11,12 @@
 
         public async Task<IEnumerable<StandardOptions>> GetOptionsForAllStandards()
         {
-            return await Get<IEnumerable<StandardOptions>>("api/v1/standards");
+            return await Get<IEnumerable<StandardOptions>>("api/v1/standards/options");
         }
 
         public async Task<StandardOptions> GetOptionsForStandard(string standard)
         {
-            return await Get<StandardOptions>($"api/v1/standards/{standard}");
+            return await Get<StandardOptions>($"api/v1/standards/options/{standard}");
         }
     }
 }
