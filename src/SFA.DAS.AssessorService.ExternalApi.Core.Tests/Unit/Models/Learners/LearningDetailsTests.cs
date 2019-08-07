6ab13b61b9c86c4f24b477a2965177a5f1368bef
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-08-07 09:01:11 +0100

    Added new LearnerDetails model for GetLearner

---
+++ src/SFA.DAS.AssessorService.ExternalApi.Core.Tests/Unit/Models/Learners/LearningDetailsTests.cs
@@ -0,0 +1,55 @@
+﻿namespace SFA.DAS.AssessorService.ExternalApi.Core.Tests.Unit.Models.Learners
+{
+    using FizzWare.NBuilder;
+    using NUnit.Framework;
+    using SFA.DAS.AssessorService.ExternalApi.Core.Models.Learners;
+    using System;
+
+    [TestFixture(Category = "Models")]
+    public class LearningDetailsTests
+    {
+        [Test]
+        public void WhenLearningDetailsValid()
+        {
+            // arrange
+            var learningDetails = Builder<LearningDetails>.CreateNew().Build();
+
+            // act
+            bool isValid = learningDetails.IsValid(out var validationResults);
+
+            // assert
+            Assert.IsTrue(isValid);
+            Assert.That(validationResults, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        [Category("IEquatable")]
+        public void WhenLearningDetailsEqual()
+        {
+            // arrange
+            var learningDetails1 = Builder<LearningDetails>.CreateNew().Build();
+            var learningDetails2 = Builder<LearningDetails>.CreateNew().Build();
+
+            // act
+            bool areEqual = learningDetails1 == learningDetails2;
+
+            // assert
+            Assert.IsTrue(areEqual);
+        }
+
+        [Test]
+        [Category("IEquatable")]
+        public void WhenLearningDetailsNotEqual()
+        {
+            // arrange
+            var learningDetails1 = Builder<LearningDetails>.CreateNew().With(ld => ld.LearningStartDate = DateTime.MinValue).Build();
+            var learningDetails2 = Builder<LearningDetails>.CreateNew().With(ld => ld.LearningStartDate = DateTime.MaxValue).Build();
+
+            // act
+            bool areNotEqual = learningDetails1 != learningDetails2;
+
+            // assert
+            Assert.IsTrue(areNotEqual);
+        }
+    }
+}
--- src/SFA.DAS.AssessorService.ExternalApi.Core/Models/Learners/LearningDetails.cs
+++ src/SFA.DAS.AssessorService.ExternalApi.Core/Models/Learners/LearningDetails.cs
@@ -4,11 +4,13 @@
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
 
-    public sealed class LearnerData : IEquatable<LearnerData>
+    public sealed class LearningDetails : IEquatable<LearningDetails>
     {
-        public Certificates.Standard Standard { get; set; }
-        public Learner Learner { get; set; }
-        public LearningDetails LearningDetails { get; set; }
+        public string LearnerReferenceNumber { get; set; }
+        public DateTime LearningStartDate { get; set; }
+        public DateTime? PlannedEndDate { get; set; }
+        public string ProviderName { get; set; }
+        public int ProviderUkPrn { get; set; }
 
         #region GetHashCode, Equals and IEquatable
         public override int GetHashCode()
@@ -19,9 +21,11 @@
                 const int multiplier = 16777619;
 
                 int hash = hashBase;
-                hash = (hash * multiplier) ^ (Standard is null ? 0 : Standard.GetHashCode());
-                hash = (hash * multiplier) ^ (Learner is null ? 0 : Learner.GetHashCode());
-                hash = (hash * multiplier) ^ (LearningDetails is null ? 0 : LearningDetails.GetHashCode());
+                hash = (hash * multiplier) ^ (LearnerReferenceNumber is null ? 0 : LearnerReferenceNumber.GetHashCode());
+                hash = (hash * multiplier) ^ (PlannedEndDate is null ? 0 : PlannedEndDate.GetHashCode());
+                hash = (hash * multiplier) ^ LearningStartDate.GetHashCode();
+                hash = (hash * multiplier) ^ (ProviderName is null ? 0 : ProviderName.GetHashCode());
+                hash = (hash * multiplier) ^ ProviderUkPrn.GetHashCode();
                 return hash;
             }
         }
@@ -31,30 +35,32 @@
             if (obj is null) return false;
             if (ReferenceEquals(this, obj)) return true;
             if (obj.GetType() != this.GetType()) return false;
-            return IsEqual((LearnerData)obj);
+            return IsEqual((LearningDetails)obj);
         }
 
-        public bool Equals(LearnerData other)
+        public bool Equals(LearningDetails other)
         {
             if (other is null) return false;
             if (ReferenceEquals(this, other)) return true;
             return IsEqual(other);
         }
 
-        private bool IsEqual(LearnerData other)
+        private bool IsEqual(LearningDetails other)
         {
-            return Equals(Standard, other.Standard)
-                && Equals(Learner, other.Learner)
-                && Equals(LearningDetails, other.LearningDetails);
+            return string.Equals(LearnerReferenceNumber, other.LearnerReferenceNumber)
+                && Equals(LearningStartDate, other.LearningStartDate)
+                && Equals(PlannedEndDate, other.PlannedEndDate)
+                && string.Equals(ProviderName, other.ProviderName)
+                && Equals(ProviderUkPrn, other.ProviderUkPrn);
         }
 
-        public static bool operator ==(LearnerData left, LearnerData right)
+        public static bool operator ==(LearningDetails left, LearningDetails right)
         {
             if (left is null) return right is null;
             return left.Equals(right);
         }
 
-        public static bool operator !=(LearnerData left, LearnerData right)
+        public static bool operator !=(LearningDetails left, LearningDetails right)
         {
             return !(left == right);
         }
